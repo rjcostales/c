80d7c56dd92a5a8e57335717c127483d7aaff96b
jesse <rjcostales@gmail.com>
2018-11-06 22:20:27 -0700

    indent

--- fibonacci.c
+++ fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long fibonacci(int n)
+long
+fibonacci (int n)
 {
-    if (n == 0)
-	return 0;
-    if (n == 1)
-	return 1;
-    return fibonacci(n - 2) + fibonacci(n - 1);
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+  return fibonacci (n - 2) + fibonacci (n - 1);
 }
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-    int n = 47;
-    if (argc == 2)
-	n = atoi(argv[1]);
-    if (n < 0 || n > 47)
-	n = 0;
+  int n = 47;
+  if (argc == 2)
+    n = atoi (argv[1]);
+  if (n < 0 || n > 47)
+    n = 0;
 
-    long f = fibonacci(n);
-    printf("fib(%i) = %lu\n", n, f);
+  long f = fibonacci (n);
+  printf ("fib(%i) = %lu\n", n, f);
 }
--- write3.c
+++ write3.c
@@ -3,31 +3,35 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-    char buffer[] =
-	" 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\n";
+  char buffer[] =
+    " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\n";
 
-    char strings[201][5];
-    char *string = &strings[0][0];
-    int rand[200];
-    int t;
+  char strings[201][5];
+  char *string = &strings[0][0];
+  int rand[200];
+  int t;
 
-    strings[200][0] = '\0';
+  strings[200][0] = '\0';
 
-    int urandom = open("/dev/urandom", O_RDONLY);
+  int urandom = open ("/dev/urandom", O_RDONLY);
 
-    for (int n = 0; n < 1000000; n++) {
-	read(urandom, rand, 800);
-	for (int i = 0; i < 200; i++) {
-	    int r = rand[i];
-	    for (int j = 0; j < 5; j++) {
-		strings[i][j] = buffer[r & 0x3f];
-		r >>= 6;
+  for (int n = 0; n < 1000000; n++)
+    {
+      read (urandom, rand, 800);
+      for (int i = 0; i < 200; i++)
+	{
+	  int r = rand[i];
+	  for (int j = 0; j < 5; j++)
+	    {
+	      strings[i][j] = buffer[r & 0x3f];
+	      r >>= 6;
 	    }
 	}
-	printf("%s", string);
+      printf ("%s", string);
     }
 
-    close(urandom);
+  close (urandom);
 }
