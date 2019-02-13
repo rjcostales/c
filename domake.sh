1c24775cde8dc53181295651854cc9c74b49292a
jesse <rjcostales@gmail.com>
2019-02-13 10:23:17 -0700

    test Makefiles

---
+++ domake.sh
@@ -0,0 +1,50 @@
+
+cd ./misc/
+make clean
+make
+make clean
+
+cd ../io/
+make clean
+
+cd ../little_c/
+make clean
+make
+make clean
+
+cd ../math/test/
+make clean
+make
+make clean
+
+cd ../fibonacci/
+make clean
+make
+make clean
+
+cd ../
+make clean
+make
+make clean
+
+cd ../8-queens/
+make clean
+make
+make clean
+
+cd ../string/
+make clean
+make
+make clean
+
+cd ../games/
+make clean
+make
+make clean
+
+cd ../sorts/
+make clean
+make
+make clean
+
+cd ..
