a6df7df53e483e2faf5a217a73b7afc02c3500a8
Jesse Costales <rjcostales@gmail.com>
2022-06-26 20:22:53 -0700

    delete work

--- work/sorts.h
+++
@@ -1,8 +0,0 @@
-#define SIZE 50000
-
-void swap(float *a, float *b);
-
-void bubble(float array[], int n);
-void insertion(float array[], int n);
-void shell(float array[], int n);
-void selection(float array[], int n);
