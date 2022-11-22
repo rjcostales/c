84517573b196e7c3325af5ea87f0c76cf34def32
Jesse Costales <rjcostales@gmail.com>
2022-11-22 13:50:05 -0700

    fix lf for shell scripts

--- fibonacci/test_fib.sh
+++ fibonacci/test_fib.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 echo "Fibonacci Test"
 echo ""
 
@@ -16,22 +15,3 @@ echo ''
 
 ./fibT 40
 echo ''
-=======
-echo "Fibonacci Test"
-echo ""
-
-./fib1 40
-echo ""
-
-./fib2 40
-echo ""
-
-./fib3 40
-echo ""
-
-./fib4 40
-echo ''
-
-./fibT 40
-echo ''
->>>>>>> 238493a (update)
